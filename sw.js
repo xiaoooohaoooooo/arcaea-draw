d5586e22e751b05d55b6f0e427221661ec30f5c6
xiaoooohaoooooo <1356819230@qq.com>
2026-09-17 12:49:28 +0800

    add sw.js

---
+++ sw.js
@@ -0,0 +1,42 @@
+/* Arcaea 抽取器 · Service Worker（离线缓存） */
+const CACHE = 'arcaea-draw-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE)
+      .then((c) => c.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+      .catch(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then((hit) => {
+      if (hit) return hit;
+      return fetch(e.request).then((res) => {
+        try {
+          const copy = res.clone();
+          caches.open(CACHE).then((c) => c.put(e.request, copy)).catch(() => {});
+        } catch (err) {}
+        return res;
+      }).catch(() => caches.match('./index.html'));
+    })
+  );
+});
